9ef24af90547ec359acb4c6a7edfb740060fd929
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-01-10 10:26:00 -0800

    Fixed function prototype

--- src/stack.h
+++ src/stack.h
@@ -20,7 +20,7 @@ struct stack {
     size_t size;        /* size of the stack */
 };
 
-struct stack *stack_create(size_t init_capacity);
+struct stack *stack_create(size_t capacity);
 void stack_push(struct stack *stack, void *data);
 void *stack_pop(struct stack *stack);
 void *stack_peek(struct stack *stack);
